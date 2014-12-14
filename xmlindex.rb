d58c92460967bd9c3465192e6c2622b615c636a1
Tristan Hume <tris.hume@gmail.com>
2014-12-14 10:42:08 -0500

    Finished xmlindex import into sqlite

--- xmlindex.rb
+++ xmlindex.rb
@@ -1,33 +1,78 @@
+require "sqlite3"
+
 class Parser
   def initialize(f)
     @f = f
+    @db = SQLite3::Database.new "xindex.db"
+    @db.execute <<-SQL
+create table pages (
+  title varchar(256) PRIMARY KEY,
+  offset int,
+  links int
+) WITHOUT ROWID;
+SQL
+    @db.execute("PRAGMA synchronous = OFF;")
+  end
+
+  def finish
+    @db.execute("PRAGMA synchronous = ON;")
   end
 
   def header
-    @f.seek(38)
+    # @f.seek(38)
+    @f.read(38)
   end
 
   def document
     match("<d>")
+    count = 0
     while @f.read(3) == "<p>"
-      page(@f)
+      page
+      count += 1
+      print '.' if count % 1000 == 0
     end
   end
 
   def page
-    match("<p><t>")
-    puts @f.gets("<") # title
+    loc = @f.pos - 3
+    match("<t>")
+    name = @f.gets("<")[0..-2] # title
     match("/t>")
+    l = links
+    match(">") # only thing left over after <l> tries to consume </p>
+    @db.execute("INSERT INTO pages (title, offset, links) VALUES (?,?,?)",[name,loc,l])
+  end
+
+  def links
+    count = 0
+    while @f.read(3) == "<l>"
+      count += 1
+      @f.gets("<")
+      match("/l>")
+    end
+    count
+  end
+
+  def page_at(i)
+    @f.seek(i)
+    match("<p>")
+    page
   end
 
   private
 
   def match(s)
-    raise "invalid" unless @f.read(s.length) == s
+    x = @f.read(s.length)
+    raise "got #{x} expected #{s}" unless x == s
   end
 end
 
 f = File.open("/Users/tristan/misc/simplewiki-links.xml")
+# f = STDIN
 p = Parser.new(f)
-p.header
-p.document
+if ARGV.length > 0
+  p.page_at(ARGV.first.to_i)
+else
+  p.header
+  p.document
+end
