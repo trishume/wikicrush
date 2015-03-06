531ca23603c278cd8c30142ba71e931d102f6049
Tristan Hume <tris.hume@gmail.com>
2015-03-05 19:47:39 -0500

    Add start of rust analysis program

--- analyze/inspect.rb
+++ analyze/inspect.rb
@@ -8,13 +8,23 @@ die "Usage: inspect.rb path/to/index.bin path/to/xindex.db query" unless ARGV.le
 
 f = File.open(ARGV[0])
 g = Graph.new(f,ARGV[1])
-q = g.find(ARGV[2])
+
+query = ARGV[2].strip
+if query.to_i.to_s == query
+  q = query.to_i
+else
+  q = g.find(query)
+end
 die "Could not find page" unless q
 
 puts "Name: #{g.name(q)}"
+puts "Index: #{q}"
 puts "Link Count: #{g.link_count(q)}"
 puts "Bidirectional Links: #{g.bi_link_count(q)}"
 puts "Bidirectional Links:"
 print_list(g, g.page_bi_links(q))
+links = g.page_links(q)
+puts "Outgoing Link Offsets:"
+p links
 puts "Outgoing Links:"
-print_list(g, g.page_un_links(q))
+print_list(g, links)
---
+++ analyze/strong_conn.rs
@@ -0,0 +1,130 @@
+#![feature(env)]
+#![feature(old_io)]
+#![feature(old_path)]
+#![feature(collections)]
+
+#![allow(dead_code)]
+
+use std::env;
+use std::mem;
+use std::old_io::File;
+
+static FILE_HEADER_SIZE : usize = 4*4;
+static PAGE_HEADER_SIZE : usize = 3;
+
+static PAGE_USER_DATA : usize = 0;
+static PAGE_LINKS : usize = 1;
+static PAGE_BID_LINKS : usize = 2;
+
+struct Graph<'a> {
+    data : &'a mut [u32],
+}
+
+impl<'a> Graph<'a> {
+    fn first_page(&self) -> usize {
+        FILE_HEADER_SIZE
+    }
+
+    fn find_next_unmarked(&self,start : usize) -> Option<usize> {
+        let mut page = start;
+        while self.user_data(page) != 0 {
+            page = page + (PAGE_HEADER_SIZE+self.link_count(page))*4;
+            if page >= self.data.len() * 4 { return None;}
+        }
+        Some(page)
+    }
+
+    fn page_count(&self) -> u32 {
+        self.data[1]
+    }
+
+    fn link_count(&self, page : usize) -> usize {
+        self.data[page/4+PAGE_LINKS] as usize
+    }
+
+    fn bid_link_count(&self, page : usize) -> usize {
+        self.data[page/4+PAGE_BID_LINKS] as usize
+    }
+
+    fn links(&'a self, page : usize) -> &'a [u32] {
+        let start = page/4+PAGE_HEADER_SIZE;
+        let end = start+self.link_count(page);
+        &self.data[start..end]
+    }
+
+    fn set_user_data(&mut self, page : usize, data : u32) {
+        self.data[page/4+PAGE_USER_DATA] = data;
+    }
+
+    fn user_data(&self, page : usize) -> u32 {
+        self.data[page/4+PAGE_USER_DATA]
+    }
+}
+
+fn flood_fill(graph : &mut Graph, start_page : usize, mark : u32) -> u32 {
+    assert!(mark != 0);
+    let mut stack = vec![start_page];
+    let mut marked_count = 0;
+    while !stack.is_empty() {
+        let page = stack.pop().unwrap();
+
+        if graph.user_data(page) != 0 {continue;}
+        graph.set_user_data(page,mark); // mark visited
+        // println!("Visiting {} with {} links",page,graph.link_count(page));
+        marked_count += 1;
+
+        for linked in graph.links(page) {
+            // println!("Pushing link to {}", linked);
+            stack.push(*linked as usize);
+        }
+    }
+    marked_count
+}
+
+fn main() {
+    let args: Vec<String> = env::args().map(|x| x.to_string()).collect();
+
+    if args.len() != 2 {
+        println!("Usage: ./strong_conn path/to/indexbi.bin");
+        env::set_exit_status(1);
+        return;
+    }
+
+    let bin_path = Path::new(&args[1]);
+    println!("Analyzing {}...",bin_path.display());
+
+    let mut file = File::open(&bin_path).ok().expect("Could not open graph file.");
+
+    let mut graph_data : Vec<u32>;
+    {
+        let mut buf : Vec<u8> = file.read_to_end().ok().expect("Could not read file.");
+        let len = buf.len();
+        println!("Read {} bytes of file!", len);
+        if len % 4 != 0 {
+            println!("Invalid file size!");
+            return;
+        }
+        let data_ptr : *mut u32 = unsafe {mem::transmute(buf.as_mut_ptr())};
+        graph_data = unsafe { Vec::from_raw_buf(data_ptr, len / 4)};
+    }
+    let mut graph = Graph { data: graph_data.as_mut_slice() };
+    println!("Read {} words of file!", graph.data.len());
+
+    let mut start_page = graph.first_page();
+    let mut comp_count = 0;
+    loop {
+        let count = flood_fill(&mut graph, start_page,1);
+        if count > 100 {
+            println!("Found a connected component of {} nodes out of {} pages = {}.",
+                     count,graph.page_count(),(count as f32 / graph.page_count() as f32));
+        }
+        comp_count += 1;
+
+        let next_page = graph.find_next_unmarked(start_page);
+        match next_page {
+            Some(page) => start_page = page,
+            None => break,
+        }
+    }
+    println!("Found {} components.",comp_count);
+}
