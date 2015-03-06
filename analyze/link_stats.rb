3a17ea3049e94fc6af819704f584768c88ec0c06
Tristan Hume <tris.hume@gmail.com>
2015-03-06 15:24:31 -0500

    Tweak link stats script

--- analyze/link_stats.rb
+++ analyze/link_stats.rb
@@ -14,17 +14,19 @@ stats = Hash.new(0)
 IO.foreach(links_path) do |line|
   page, *links = line.chomp.split('|').map{ |x| x.strip }
   stats[:pages] += 1
-  stats[:valid_pages] += 1 if valid.has_key?(page)
-  stats[:links] += links.count
+  if valid.has_key?(page)
+    stats[:valid_pages] += 1
+    stats[:links] += links.count
 
-  links_uniq = links.uniq
-  stats[:unique_links] += links_uniq.count
-  stats[:valid_links] += links_uniq.count { |l| valid.has_key?(l.capitalize)}
+    links_uniq = links.uniq
+    stats[:unique_links] += links_uniq.count
+    stats[:valid_links] += links_uniq.count { |l| valid.has_key?(l.capitalize)}
+  end
 end
 stats[:valid_link_frac] = stats[:valid_links] / stats[:unique_links].to_f
 stats[:valid_page_frac] = stats[:valid_pages] / stats[:pages].to_f
 [:links,:unique_links,:valid_links].each do |stat|
   avg_name = ("average_" + stat.to_s).intern
-  stats[avg_name] = stats[stat] / stats[:pages].to_f
+  stats[avg_name] = stats[stat] / stats[:valid_pages].to_f
 end
 p stats
